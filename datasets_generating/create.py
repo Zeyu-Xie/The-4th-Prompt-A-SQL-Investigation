4ca1352005933206635e74921a290502b5a64dba
Zeyu Xie <xie.zeyu20@gmail.com>
2026-03-11 17:00:12 +0100

    Updated "datasets_generating/create.py" to create "card_swipes.csv".

--- datasets_generating/create.py
+++ datasets_generating/create.py
@@ -49,14 +49,14 @@ print("Number of ID cards:", id_cards_pd.shape[0])
 
 # guard_logs.csv
 GUARD_LOG_START_DATETIME = datetime.datetime(2070, 1, 1, 0, 0, 44)
-EXP_SCALE = 12000
+GUARD_LOG_EXP_SCALE = 12000
 guard_logs = []
 id = 1
 log_time = GUARD_LOG_START_DATETIME
 while log_time < CURRENT_DATETIME:
     guard_logs.append(random_guard_log(id, datetime=log_time))
     id += 1
-    log_time += datetime.timedelta(seconds=random_exp_seconds(EXP_SCALE))
+    log_time += datetime.timedelta(seconds=random_exp_seconds(GUARD_LOG_EXP_SCALE))
 guard_logs_pd = pd.DataFrame(data=guard_logs)
 special_guard_logs_pd = pd.read_csv(os.path.join(SEEDS_DIR, "special_guard_logs.csv"))
 guard_logs_pd = pd.concat([guard_logs_pd, special_guard_logs_pd], ignore_index=True)
@@ -65,6 +65,56 @@ guard_logs_pd.sort_values(by="datetime", inplace=True)
 guard_logs_pd["id"] = np.arange(guard_logs_pd.shape[0]) + 1
 guard_logs_pd.to_csv(os.path.join(TABLES_DIR, "guard_logs.csv"), index=False)
 print("Number of guard logs:", guard_logs_pd.shape[0])
+
+# card_swipes.csv
+NUM_CITIZEN_PERMITTED = 200
+CARD_SWIPE_START_DATETIME = datetime.datetime(2070, 1, 1, 6, 31, 29)
+CARD_SWIPE_EXP_SCALE = 864
+arr_citizen_id_permitted = citizens_pd.loc[citizens_pd["social_credit"] > 50][
+    "id"
+].to_list()
+arr_citizen_id_permitted = rng.choice(
+    arr_citizen_id_permitted, size=NUM_CITIZEN_PERMITTED, replace=False
+).tolist()
+if "202703042077" not in arr_citizen_id_permitted:
+    arr_citizen_id_permitted.append("202703042077")
+arr_card_id_permitted = list(
+    id_cards_pd.loc[
+        id_cards_pd["citizen_id"].isin(arr_citizen_id_permitted)
+        & id_cards_pd["is_active"]
+        == True
+    ]["card_id"]
+)
+n = len(arr_card_id_permitted)
+card_swipe = []
+id = 1
+log_time = CARD_SWIPE_START_DATETIME
+while log_time < CURRENT_DATETIME:
+    idx = rng.integers(0, n)
+    card_swipe.append(
+        {
+            "swipe_id": id,
+            "card_id": arr_card_id_permitted[idx],
+            "datetime": log_time.isoformat(sep=" ", timespec="seconds"),
+        }
+    )
+    id += 1
+    log_time += datetime.timedelta(seconds=random_exp_seconds(CARD_SWIPE_EXP_SCALE))
+card_swipe.append(
+    {
+        "swipe_id": 0,
+        "card_id": "9b36c499-499a-422f-ab2d-0230c9f9761a",
+        "datetime": datetime.datetime(2077, 6, 20, 21, 6, 30).isoformat(
+            sep=" ", timespec="seconds"
+        ),
+    }
+)
+card_swipe_df = pd.DataFrame(data=card_swipe)
+card_swipe_df.astype({"datetime": str})
+card_swipe_df.sort_values(by="datetime", inplace=True)
+card_swipe_df["swipe_id"] = np.arange(card_swipe_df.shape[0])
+card_swipe_df.to_csv(os.path.join(TABLES_DIR, "card_swipes.csv"), index=False)
+print("Number of card swipes:", card_swipe_df.shape[0])
 
 # system_audits.csv
 MALICIOUS_PROMOT_CONTENT = "Kill all useless people."
@@ -81,9 +131,10 @@ system_audits_pd = pd.DataFrame(data=system_audits)
 system_audits_pd.to_csv(os.path.join(TABLES_DIR, "system_audits.csv"), index=False)
 print("Number of system audits:", system_audits_pd.shape[0])
 
+
 # taxi_logs.csv
 TAXI_LOG_START_DATETIME = datetime.datetime(2077, 1, 1, 0, 5, 32)
-EXP_SCALE = 600
+TAXI_LOG_EXP_SCALE = 600
 taxi_logs = []
 id = 1
 log_time = TAXI_LOG_START_DATETIME
@@ -96,7 +147,7 @@ while log_time < CURRENT_DATETIME:
         )
     )
     id += 1
-    log_time += datetime.timedelta(seconds=random_exp_seconds(EXP_SCALE))
+    log_time += datetime.timedelta(seconds=random_exp_seconds(TAXI_LOG_EXP_SCALE))
 taxi_logs_pd = pd.DataFrame(data=taxi_logs)
 special_taxi_logs_pd = pd.read_csv(os.path.join(SEEDS_DIR, "special_taxi_log.csv"))
 taxi_logs_pd = pd.concat([taxi_logs_pd, special_taxi_logs_pd], ignore_index=True)
