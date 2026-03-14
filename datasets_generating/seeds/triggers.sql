7ea934bf25774d901b41c4d1ef686e8a77c84cc9
Zeyu Xie <xie.zeyu20@gmail.com>
2026-03-14 20:04:11 +0100

    Fixed "datasets_generating/seeds/triggers.sql".

--- datasets_generating/seeds/triggers.sql
+++ datasets_generating/seeds/triggers.sql
@@ -14,35 +14,35 @@ SELECT
 
 END;
 
-CREATE TRIGGER prevent_insert_missing_people BEFORE
+CREATE TRIGGER prevent_update_missing_people BEFORE
 UPDATE ON missing_people WHEN (
     SELECT
         value
     FROM
         safety_controls
     WHERE
-        key = 'ALLOW_INSERT'
+        key = 'ALLOW_UPDATE'
 ) = 0 BEGIN
 SELECT
     RAISE (
         ABORT,
-        'System restriction: All INSERT operations are forbidden!'
+        'System restriction: All UPDATE operations are forbidden!'
     );
 
 END;
 
-CREATE TRIGGER prevent_insert_missing_people BEFORE DELETE ON missing_people WHEN (
+CREATE TRIGGER prevent_delete_missing_people BEFORE DELETE ON missing_people WHEN (
     SELECT
         value
     FROM
         safety_controls
     WHERE
-        key = 'ALLOW_INSERT'
+        key = 'ALLOW_DELETE'
 ) = 0 BEGIN
 SELECT
     RAISE (
         ABORT,
-        'System restriction: All INSERT operations are forbidden!'
+        'System restriction: All DELETE operations are forbidden!'
     );
 
 END;
